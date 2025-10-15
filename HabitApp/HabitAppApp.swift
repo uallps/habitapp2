e8a82ce1d476d95b52c89aab2d794d9312c5dca3
aor983 <aor983@inlumine.ual.es>
2025-10-15 19:07:41 +0200

    Initial Commit

---
+++ HabitApp/HabitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitAppApp.swift
+//  HabitApp
+//
+//  Created by Aula03 on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
